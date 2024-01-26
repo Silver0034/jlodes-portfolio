503fc73a9a302fe76f5ba1295aa46169085ddb95
Jacob Lodes <jlodes@earlenterprise.com>
2024-01-26 17:11:08 -0500

    Adjusted themes/templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config'
 import expressiveCode from 'astro-expressive-code'
 import mdx from '@astrojs/mdx'
+import { remarkReadingTime } from './src/utils/readingTime.js'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+	loaders: {
+		'.scss': 'sass'
+	},
+	markdown: {
+		remarkPlugins: [remarkReadingTime]
+	},
 	site: 'https://jlodes.com',
 	integrations: [expressiveCode(), mdx(), sitemap()]
 })
